b4b4fbf20136bb7de96a7e1d3f6f8cb89d5ceeea
Mateo <mateo@lunchtimelabs.io>
2024-07-31 11:25:19 -0400

    add supabaseClient + config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
@@ -7,5 +7,6 @@ export default defineConfig({
   plugins: [
     react(),
     // eslint()
-  ]
+  ],
+  envPrefix: 'FRONTEND_',
 });
