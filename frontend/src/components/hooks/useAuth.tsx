3364ee4dd5541f4171004bb6b51b61a9fcda517e
Mateo <mateo@lunchtimelabs.io>
2024-08-01 13:41:43 -0400

    useAuth with authChangeEvent

--- frontend/src/components/hooks/useAuth.tsx
+++ frontend/src/components/hooks/useAuth.tsx
@@ -5,6 +5,7 @@ import supabase from '../../supabaseClient.ts';
 interface AuthContext {
   session: Session | null;
   user: User | null;
+  authChangeEvent: AuthChangeEvent | null;
 }
 
 const initialState = { session: null, user: null } as AuthContext;
@@ -13,6 +14,7 @@ const authContext = React.createContext(initialState);
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<Session | null>(null);
   const [user, setUser] = useState<User | null>(session?.user ?? null);
+  const [authChangeEvent, setAuthEvent] = useState<AuthChangeEvent | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -25,6 +27,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       async (_event: AuthChangeEvent, _session: Session | null) => {
         setSession(_session);
         setUser(_session?.user ?? null);
+        setAuthEvent(_event);
       },
     );
 
@@ -34,7 +37,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   }, []);
 
   // eslint-disable-next-line react/jsx-no-constructed-context-values
-  const value = { session, user };
+  const value = { authChangeEvent, session, user };
   return <authContext.Provider value={value}>{children}</authContext.Provider>;
 }
 
