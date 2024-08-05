ec6d304f509a5e3f45051f8775ae063db7bb46b8
Mateo <mateo@lunchtimelabs.io>
2024-08-05 09:53:08 -0400

    password reset

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -5,6 +5,7 @@ import { Auth } from '@supabase/auth-ui-react';
 import { useAuth } from '../components/hooks/useAuth.tsx';
 import supabase from '../supabaseClient.ts';
 import customTheme from '../authCustomTheme.ts';
+import authChangeEvents from '../authChangeEvents.ts';
 
 // images
 import logoUrl from '../../timething.svg';
@@ -17,7 +18,7 @@ import '../../css/pages/HomePage.css';
  *
  */
 export default function HomePage() {
-  const { session } = useAuth();
+  const { session, user, authChangeEvent } = useAuth();
   const { state } = useLocation();
   const navigate = useNavigate();
 
@@ -28,6 +29,8 @@ export default function HomePage() {
   const signOut = () => {
     supabase.auth.signOut();
   };
+
+  const isSignedIn = session && user && authChangeEvent === authChangeEvents.SIGNED_IN;
 
   return (
     <div className="min-w-full min-h-screen screen">
@@ -47,7 +50,7 @@ export default function HomePage() {
             video files with just one upload. Enhance accessibility, increase
             comprehension and break language barriers with subtitles.
           </h2>
-          {session ? (
+          {isSignedIn ? (
             <div id="buttons">
               <div className="button">
                 <label className="btn btn-lg btn-primary" onClick={upload}>
